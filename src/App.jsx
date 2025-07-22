1b3b07ca8616bb1f1bc7fd691f348a67edc779d9
GlendyT <glendytuyuc@gmail.com>
2025-07-22 16:25:58 -0600

    finishing the experience section

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
-import React from "react";
 import Hero from "./sections/Hero";
+import ShowcaseSection from "./sections/ShowcaseSection";
+import Navbar from "./components/Navbar";
+import LogoSection from "./sections/LogoSection";
+import FeatureCards from "./sections/FeatureCards";
+import ExperienceSection from "./sections/ExperienceSection";
 
 const App = () => {
   return (
     <>
+      <Navbar />
       <Hero />
+      <ShowcaseSection />
+      <LogoSection />
+      <FeatureCards />
+      <ExperienceSection />
     </>
   );
 };
